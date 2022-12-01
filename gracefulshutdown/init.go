5efe6b98ae819561bfc3c8b37eed837e558845a4
eric_liu <eric_liu@paradise-soft.com.tw>
2022-12-01 18:04:28 +0800

    update graceful shutdown

--- gracefulshutdown/init.go
+++ gracefulshutdown/init.go
@@ -3,7 +3,6 @@ package gracefulshutdown
 import (
 	"context"
 	"log"
-	"math"
 	"os"
 	"os/signal"
 	"sync"
@@ -17,18 +16,19 @@ var osShutdownChan *chan os.Signal
 type srvShutdownChan chan srvSignal
 type srvSignal struct{}
 
-func Init() {
+// Initialize gracefulshutdown
+func Init(srvNum int) {
 	osShutdownChan = new(chan os.Signal)
 	// kill no param => syscall.SIGTERM
 	// kill -2 => syscall.SIGINT, ctrl + c, os.Interrupt
 	// kill -9 => syscall.SIGKILL
 	signal.Notify(*osShutdownChan, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
 
-	cancels := make([]context.CancelFunc, math.MaxUint8)
-	ctxs = make([]context.Context, math.MaxUint8)
-	wgs = make([]*sync.WaitGroup, math.MaxUint8)
+	cancels := make([]context.CancelFunc, srvNum)
+	ctxs = make([]context.Context, srvNum)
+	wgs = make([]*sync.WaitGroup, srvNum)
 
-	for i := 0; i < math.MaxUint8; i++ {
+	for i := 0; i < srvNum; i++ {
 		ctxs[i], cancels[i] = context.WithCancel(context.Background())
 		wgs[i] = new(sync.WaitGroup)
 	}
@@ -36,7 +36,7 @@ func Init() {
 	go func() {
 		<-*osShutdownChan
 		signal.Stop(*osShutdownChan)
-		for i := 0; i < math.MaxUint8; i++ {
+		for i := 0; i < srvNum; i++ {
 			cancels[i]()
 			wgs[i].Wait()
 		}
